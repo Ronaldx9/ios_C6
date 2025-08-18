d6e84cba7d30d734c70a6647e069f59d7174aabe
Ronaldx9 <83137449+Ronaldx9@users.noreply.github.com>
2025-08-18 09:55:35 -0300

    Add files via upload

---
+++ OLAMUNDO.java
@@ -0,0 +1,9 @@
+package Aula01_Introducao;
+
+public class OLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("OLÁ MUNDooooooO!")
+        ;
+    }
+}
---
+++ Olamundo2.java
@@ -0,0 +1,9 @@
+package Aula01_Introducao;
+
+public class OLAMUNDO2 {
+
+    public static void main(String[] args) {
+        System.out.println("OLÁ MUNDooooooOyyyyyy!")
+        ;
+    }
+}
