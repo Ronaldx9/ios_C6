6eb7c23592544f3cb4f0900b6db6f2660c0328a2
Ronaldx9 <83137449+Ronaldx9@users.noreply.github.com>
2025-08-19 09:45:10 -0300

    Add files via upload

---
+++ Olamundo2.java
@@ -0,0 +1,9 @@
+package Aula01_Introducao;
+
+public class Olamundo2 {
+//vocme
+    public static void main(String[] args) {
+        System.out.println("OLÁ MUNDooooooOyyyyyy!")
+        ;
+    }
+}
