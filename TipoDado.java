a81200dec937de288a0bab7ce8b6df7f825442a1
Ronaldx9 <83137449+Ronaldx9@users.noreply.github.com>
2025-08-19 08:55:21 -0300

    Add files via upload

---
+++ TipoDado.java
@@ -0,0 +1,10 @@
+package Aula02_Tiposdedados;
+
+public class TipoDado {
+    public static void main(String[] args) {
+        String nome = "Ronaldo" ;
+        int idade = 40 ;
+
+    System.out.println("Olá " + nome + ", você tem " + idade + " anos.");    
+    }
+}
