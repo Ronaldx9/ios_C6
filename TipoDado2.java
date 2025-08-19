ae464dbfbf6bc2d16ba8f26d59bf901642bd673d
Ronaldx9 <83137449+Ronaldx9@users.noreply.github.com>
2025-08-19 09:45:42 -0300

    Add files via upload

---
+++ TipoDado2.java
@@ -0,0 +1,24 @@
+package Aula02_Tiposdedados;
+
+public class TipoDado2 {
+    public static void main(String[] args) {
+        String nome = "Jonattan" ;
+        String nome2 = "Rica" ;
+
+        int idade = 30 ;
+        int idade2 = 25 ;
+
+        int nota1 = 15;
+        int nota2 = 5;
+
+        int media = (nota1 + nota2)/2 ;
+
+        System.out.println("Olá " + nome + ", você tem " + idade + " anos.");    
+    
+        System.out.println("Olá " + nome2 + ", você tem " + idade2 + " anos.");   
+    
+        System.out.println("Nota: " + media);  
+
+
+}
+}
