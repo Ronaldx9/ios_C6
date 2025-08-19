d57b48fa124d74dbd551970e83d7a4cfa13a0bad
Ronaldx9 <83137449+Ronaldx9@users.noreply.github.com>
2025-08-19 09:47:23 -0300

    Completo

--- TipoDado.java
+++ TipoDado.java
@@ -3,8 +3,36 @@ package Aula02_Tiposdedados;
 public class TipoDado {
     public static void main(String[] args) {
         String nome = "Ronaldo" ;
-        int idade = 40 ;
+        String nome2 = "Júlia" ;
 
-    System.out.println("Olá " + nome + ", você tem " + idade + " anos.");    
-    }
+        int idade = 40 ;
+        int idade2 = 22 ;
+
+        int nota1 = 15;
+        int nota2 = 5;
+
+        int media = (nota1 + nota2)/2 ;
+
+        System.out.println("Olá " + nome + ", você tem " + idade + " anos.");    
+    
+        System.out.println("Olá " + nome2 + ", você tem " + idade2 + " anos.");   
+    
+        System.out.println("Nota: " + media);  
+
+
+        if (media < 5 ) {
+            System.out.println("Você está reprovado!");
+        }
+        
+        else if (media <= 7 ) {
+            System.out.println("Você está de recuperação!");
+        } 
+        
+        else {
+            System.out.println("Você está aprovado :).");
+    
 }
+
+}
+}
+   
